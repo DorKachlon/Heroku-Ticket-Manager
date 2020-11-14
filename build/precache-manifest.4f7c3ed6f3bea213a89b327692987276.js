c096c18305421e03960c6512c9a9b7c733ce53f3
DorKachlon <67858139+DorKachlon@users.noreply.github.com>
2020-11-14 16:27:10 +0200

    Initial commit

---
+++ build/precache-manifest.4f7c3ed6f3bea213a89b327692987276.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e48ad88df3a096eae101b32b626eaf28",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e6eae957aa3214abdd84",
+    "url": "/static/css/main.b3755654.chunk.css"
+  },
+  {
+    "revision": "2b78bfdce968529d13d0",
+    "url": "/static/js/2.1099012b.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.1099012b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6eae957aa3214abdd84",
+    "url": "/static/js/main.938bb52e.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
